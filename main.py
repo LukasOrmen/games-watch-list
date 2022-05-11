67ec1269f13e139ad6ffaeebcf29632c57eb0090
LukasOrmen <lukaso922o@gmail.com>
2022-05-11 20:47:05 +0200

    Imporoved settings, no longer using weeks but instead game names. Added end dates for the games

--- main.py
+++ main.py
@@ -1,49 +1,48 @@
-from datetime import datetime
-from datetime import date
+from datetime import datetime, date
 import requests
 import json
 
+#loading settings
+with open("settings.json") as f:
+    settings = json.loads(f.read())
+
+#loading and parsing the API json
+r = json.loads(requests.get("https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?").text)
+all_games = r["data"]["Catalog"]["searchStore"]["elements"]
+
+titles = []
+
+print("--- Free games in epic games store ---")
+for game in all_games:
+    title = game["title"]
+    price = game["price"]["totalPrice"]["fmtPrice"]["discountPrice"]
+    promotions = game["promotions"]
+
+
+    if price == "0" and promotions != None: # Limted free games
+        print(title)
+        titles.append(title)
+        raw_enddate = game["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]["endDate"]
+        encoded_enddate = datetime.strptime(raw_enddate, '%Y-%m-%dT%H:%M:%S.%fZ').strftime("%Y-%#m-%#d").split("-")
+        final_encoded_date = date(int(encoded_enddate[0]), int(encoded_enddate[1]), int(encoded_enddate[2]))
+
+        startdate = datetime.now().strftime("%Y-%#m-%#d").split("-")
+        currentDate = date(int(startdate[0]), int(startdate[1]), int(startdate[2]))
+
+        days_left = final_encoded_date - currentDate
+        print(str(days_left).split(",")[0] + " left of the offer.\n")
+
+if settings["settings"]["alwaysshow"] == True:
+    input()
+else:
+    common = str(set(titles) & set(settings["games"]["epicgames"]))
+    if common == "set()":
+        input()
+
 with open("settings.json", "r") as f:
     data = json.loads(f.read())
-
-
-if not data["weeknum"] == int(datetime.today().strftime('%V')):
-    r = json.loads(requests.get("https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?").text)
-    all_games = r["data"]["Catalog"]["searchStore"]["elements"]
-
-    limited = []
-    freegames = []
-
-    for game in all_games:
-        title = game["title"]
-        price = game["price"]["totalPrice"]["fmtPrice"]["discountPrice"]
-        original_price = game["price"]["totalPrice"]["originalPrice"]
-        promotions = game["promotions"]
-
-        if price == "0" and promotions != None:
-            limited.append(title)
-        elif price == "0":
-            freegames.append(title)
-
-    print("--- Limited Free Games ---")
-    for games in limited:
-
-        year = date.today().strftime("%Y")
-        month = date.today().strftime("%m")
-        day = date.today().strftime("%d")
-        startDate = date(int(year), int(month), int(day))
-        endDate = date(int(games["effectiveDate"].split("T")[0]))
-        diff = startDate - endDate
-        print(diff.days)
-
-    print("\n\n--- Non Limited Free Games ---")
-    for games in freegames:
-        print(games)
-
-
-#updating week number
-    with open("settings.json", "r") as f:
-        data = json.loads(f.read())
-    data["weeknum"] = int(datetime.today().strftime('%V')) + 1
-    with open("settings.json", "w") as f:
-        f.write(json.dumps(data))
+for t in titles:
+    if not t in settings["games"]["epicgames"]:
+        data["games"]["epicgames"].append(t)
+with open("settings.json", "w") as f:
+    f.write(json.dumps(data))
